7591e517c874a2d390de518e5b8c01e1040b8f99
carterhanks <carterhanks@gmail.com>
2021-06-15 17:58:37 -0600

    Partial Code

--- server/index.js
+++ server/index.js
@@ -19,14 +19,3 @@ const server_port = 4004;
 app.listen(server_port, function() {
     console.log(`Server Sredding on ${server_port}`)
 });
-
-// let existingHouse = req.params.houseId;
-// let newHouse = req.body.houseId;
-// for (let i = 0; i < houses.length; i++); {
-//     if (houses[i].houseId === existingHouse) {
-//         houses[i].houseId = newHouse;
-//         res.status(200).send('House Updated Successfully!');
-//         return
-//     }
-// }
-// res.status(400).send("House not Found.")
